73e80f37579396698499ffd809873a9cf91702b0
tgrundle <tgrundlesr@gmail.com>
2023-12-17 21:00:13 +0000

    Initial attempt are runnig as service user

--- stop.sh
+++ stop.sh
@@ -17,8 +17,14 @@ if [[ $(id -u) = 0 ]]; then
   exit 1
 fi
 
+if [[ "$USER" = "userxname" ]]; then
+  SCREEN_CMD="screen"
+else 
+  SCREEN_CMD="sudo -u userxname screen"
+fi
+
 # Check if server is running
-if ! screen -list | grep -q '\.servername\s'; then
+if ! $SCREEN_CMD -list | grep -q '\.servername\s'; then
   echo "Server is not currently running!"
   exit 1
 fi
@@ -47,18 +53,18 @@ done
 # Stop the server
 while [[ $CountdownTime -gt 0 ]]; do
   if [[ $CountdownTime -eq 1 ]]; then
-    screen -Rd servername -X stuff "say Stopping server in 60 seconds...$(printf '\r')"
+    $SCREEN_CMD -Rd servername -X stuff "say Stopping server in 60 seconds...$(printf '\r')"
     echo "Stopping server in 60 seconds..."
     sleep 30
-    screen -Rd servername -X stuff "say Stopping server in 30 seconds...$(printf '\r')"
+    $SCREEN_CMD -Rd servername -X stuff "say Stopping server in 30 seconds...$(printf '\r')"
     echo "Stopping server in 30 seconds..."
     sleep 20
-    screen -Rd servername -X stuff "say Stopping server in 10 seconds...$(printf '\r')"
+    $SCREEN_CMD -Rd servername -X stuff "say Stopping server in 10 seconds...$(printf '\r')"
     echo "Stopping server in 10 seconds..."
     sleep 10
     CountdownTime=$((CountdownTime - 1))
   else
-    screen -Rd servername -X stuff "say Stopping server in $CountdownTime minutes...$(printf '\r')"
+    $SCREEN_CMD -Rd servername -X stuff "say Stopping server in $CountdownTime minutes...$(printf '\r')"
     echo "Stopping server in $CountdownTime minutes...$(printf '\r')"
     sleep 60
     CountdownTime=$((CountdownTime - 1))
@@ -66,13 +72,13 @@ while [[ $CountdownTime -gt 0 ]]; do
   echo "Waiting for $CountdownTime more minutes ..."
 done
 echo "Stopping Minecraft server ..."
-screen -Rd servername -X stuff "say Stopping server (stop.sh called)...$(printf '\r')"
-screen -Rd servername -X stuff "stop$(printf '\r')"
+$SCREEN_CMD -Rd servername -X stuff "say Stopping server (stop.sh called)...$(printf '\r')"
+$SCREEN_CMD -Rd servername -X stuff "stop$(printf '\r')"
 
 # Wait up to 20 seconds for server to close
 StopChecks=0
 while [[ $StopChecks -lt 20 ]]; do
-  if ! screen -list | grep -q '\.servername\s'; then
+  if ! $SCREEN_CMD -list | grep -q '\.servername\s'; then
     break
   fi
   sleep 1
@@ -80,9 +86,9 @@ while [[ $StopChecks -lt 20 ]]; do
 done
 
 # Force quit if server is still open
-if screen -list | grep -q '\.servername\s'; then
+if $SCREEN_CMD -list | grep -q '\.servername\s'; then
   echo "Minecraft server still hasn't stopped after 20 seconds, closing screen manually"
-  screen -S servername -X quit
+  $SCREEN_CMD -S servername -X quit
 fi
 
 echo "Minecraft server servername stopped."
